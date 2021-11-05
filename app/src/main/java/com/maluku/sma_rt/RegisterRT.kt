66bc0bfcefa2b22f7941e39304d77446c2dc3160
albertusadrian7 <albertsadrian7@gmail.com>
2021-11-05 14:33:00 +0700

    Menambahkan Fitur Registrasi Admin (RT)

--- app/src/main/java/com/maluku/sma_rt/RegisterRT.kt
+++ app/src/main/java/com/maluku/sma_rt/RegisterRT.kt
@@ -5,55 +5,57 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import com.maluku.sma_rt.databinding.FragmentRegisterRTBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [RegisterRT.newInstance] factory method to
- * create an instance of this fragment.
- */
 class RegisterRT : Fragment() {
     // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentRegisterRTBinding
+    private lateinit var inputIdRT: String
+    private lateinit var inputNamaAdmin: String
+    private lateinit var inputAlamatAdmin: String
+    private lateinit var inputGenderAdmin: String
+    private lateinit var inputNoHpAdmin: String
+    private lateinit var inputConfirmPassword: String
+    private lateinit var inputPassword: String
+    private lateinit var inputKodePos: String
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register_r_t, container, false)
+        binding = FragmentRegisterRTBinding.inflate(layoutInflater)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment RegisterRT.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            RegisterRT().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val genderList = listOf("Laki-Laki", "Perempuan")
+        val adapter = ArrayAdapter<String>(context!!, R.layout.support_simple_spinner_dropdown_item,genderList)
+        var isSpinnerInitial = true
+        binding.spGenderAdmin.adapter = adapter
+        inputIdRT = binding.inputIdRT.toString()
+        inputNamaAdmin = binding.inputNamaAdmin.toString()
+        inputAlamatAdmin = binding.inputAlamatAdmin.toString()
+        inputNoHpAdmin = binding.inputNoHpAdmin.toString()
+        inputPassword = binding.inputPassword.toString()
+        inputConfirmPassword = binding.inputConfirmPassword.toString()
+        inputKodePos = binding.inputKodePos.toString()
+        binding.spGenderAdmin.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(adapterView: AdapterView<*>?, view: View?, positiion: Int, id: Long) {
+                if (isSpinnerInitial){
+                    isSpinnerInitial = false
+                } else {
+                    Toast.makeText(context,"You selected ${adapterView?.getItemAtPosition(positiion).toString()}",Toast.LENGTH_LONG).show()
                 }
             }
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+
+            }
+        }
     }
+
 }
