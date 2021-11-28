89edef5bc165419dc1376e23b2f8a8219cf444af
abghifareihand <info.abghifareihan@gmail.com>
2021-11-29 01:53:43 +0700

    Update

--- app/src/main/java/com/maluku/sma_rt/view/activity/DashboardWargaActivity.kt
+++ app/src/main/java/com/maluku/sma_rt/view/activity/DashboardWargaActivity.kt
@@ -11,17 +11,21 @@ import com.maluku.sma_rt.databinding.ActivityDashboardWargaBinding
 
 class DashboardWargaActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityDashboardWargaBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityDashboardWargaBinding.inflate(layoutInflater)
-        setContentView(binding.root)
 
-        val navView: BottomNavigationView = binding.bottomNav
+        installSplashScreen()
+        setContentView(R.layout.activity_dashboard_warga)
+
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_nav)
         val navController = findNavController(R.id.fragment_dashboard_warga)
 
-        navView.setupWithNavController(navController)
-        navView.itemIconTintList = null
+
+        bottomNav.setupWithNavController(navController)
+        bottomNav.setItemIconTintList(null)
+
+
     }
+
 }
