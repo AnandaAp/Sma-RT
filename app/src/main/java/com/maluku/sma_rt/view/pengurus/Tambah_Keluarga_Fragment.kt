4e46343b4c13fd529725447650dbefa49e96c118
gilanghrlmbng <gilanghrlmbng03@gmail.com>
2021-11-29 01:38:12 +0700

    fixing_warga_fragment_and_add_button_logout_admin_rt_and_fixing_navbar_rt_and_add_icons_in_manage_page

--- app/src/main/java/com/maluku/sma_rt/view/pengurus/Tambah_Keluarga_Fragment.kt
+++ app/src/main/java/com/maluku/sma_rt/view/pengurus/Tambah_Keluarga_Fragment.kt
@@ -1,4 +1,4 @@
-package com.maluku.sma_rt.view
+package com.maluku.sma_rt.view.pengurus
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -14,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [loginFragment.newInstance] factory method to
+ * Use the [Tambah_Keluarga_Fragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class LoginChoiceFragment : Fragment() {
+class Tambah_Keluarga_Fragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -35,7 +35,7 @@ class LoginChoiceFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login_choice, container, false)
+        return inflater.inflate(R.layout.fragment_tambah_keluarga, container, false)
     }
 
     companion object {
@@ -45,12 +45,12 @@ class LoginChoiceFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment loginFragment.
+         * @return A new instance of fragment Tambah_Keluarga_Fragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            LoginChoiceFragment().apply {
+            Tambah_Keluarga_Fragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
