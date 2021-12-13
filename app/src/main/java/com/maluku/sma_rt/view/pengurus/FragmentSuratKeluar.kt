a0b2cf190f03b24f67503cd2fa1f47ac84354660
albertusadrians <albertus.adrian@ti.ukdw.ac.id>
2021-12-13 11:42:52 +0700

    Fitur untuk persuratan pada pengurus RT

--- app/src/main/java/com/maluku/sma_rt/view/pengurus/FragmentSuratKeluar.kt
+++ app/src/main/java/com/maluku/sma_rt/view/pengurus/FragmentSuratKeluar.kt
@@ -5,56 +5,38 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.maluku.sma_rt.R
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.maluku.sma_rt.databinding.FragmentSuratKeluarBinding
+import com.maluku.sma_rt.view.pengurus.adapter.SuratKeluarAdapter
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [FragmentSuratKeluar.newInstance] factory method to
- * create an instance of this fragment.
- */
 class FragmentSuratKeluar : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentSuratKeluarBinding
+    private lateinit var rvSuratKeluar: RecyclerView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_surat__keluar, container, false)
+    ): View {
+        val view = bindingView()
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment FragmentSurat_Keluar.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            FragmentSuratKeluar().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setRecyclerViewSuratKeluar()
+    }
+
+    private fun setRecyclerViewSuratKeluar() {
+        val suratKeluarAdapter = SuratKeluarAdapter()
+        rvSuratKeluar = binding.rvSuratKeluar
+        rvSuratKeluar.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL ,false)
+        rvSuratKeluar.adapter = suratKeluarAdapter
+    }
+
+    private fun bindingView(): View {
+        binding = FragmentSuratKeluarBinding.inflate(layoutInflater)
+        return binding.root
     }
 }
