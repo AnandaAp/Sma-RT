ccdc98af38e84ba5461fb24eb8587b9d3069b090
bantolos <bantolo.setiadi@ti.ukdw.ac.id>
2021-12-31 00:07:21 +0700

    detail info ok

--- app/src/main/java/com/maluku/sma_rt/view/warga/adapter/RecyclerViewInfoTerkini.kt
+++ app/src/main/java/com/maluku/sma_rt/view/warga/adapter/RecyclerViewInfoTerkini.kt
@@ -1,16 +1,23 @@
 package com.maluku.sma_rt.view.warga.adapter
 
 import android.content.ContentValues
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.Window
+import android.widget.ImageButton
 import android.widget.ImageView
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.CenterCrop
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.bumptech.glide.request.RequestOptions
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.firebase.storage.FirebaseStorage
 import com.maluku.sma_rt.R
 import com.maluku.sma_rt.model.informasi.GetAllInformasiItem
@@ -34,6 +41,8 @@ class RecyclerViewInfoTerkini(
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         val data = listInfo[position]
+
+        holder.judul.text = data.judul.toString()
         // Firebase Storage
         val storageRef = FirebaseStorage.getInstance().reference.child("images/${data.gambar}")
         Log.d(ContentValues.TAG,"Adapter get ref image: $storageRef")
@@ -47,6 +56,38 @@ class RecyclerViewInfoTerkini(
 
         }
 
+        holder.layoutInfoTerkini.setOnClickListener { view ->
+            val dialog = BottomSheetDialog(view.context, R.style.AppBottomSheetDialogTheme)
+            dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+            dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+            dialog.setContentView(R.layout.bottomsheet_detailinformasi)
+
+            val judul = dialog.findViewById<TextView>(R.id.namainformasi)
+            val gambar = dialog.findViewById<ImageView>(R.id.gambarinformasi)
+            val deskripsi = dialog.findViewById<TextView>(R.id.deskripsiinformasi)
+
+            judul!!.text = data.judul.toString()
+            deskripsi!!.text = data.detail.toString()
+            val storageRef = FirebaseStorage.getInstance().reference.child("images/${data.gambar}")
+            Log.d(ContentValues.TAG,"Adapter get ref image: $storageRef")
+            val localFile = File.createTempFile("tempFile","jpg")
+            storageRef.getFile(localFile).addOnSuccessListener {
+                Glide.with(holder.itemView)
+                    .load(localFile.path)
+                    .apply(RequestOptions().transform(CenterCrop(), RoundedCorners(20)))
+                    .into(gambar!!)
+            }.addOnFailureListener {
+            }
+
+            val btnClose = dialog.findViewById<ImageButton>(R.id.btn_close)
+
+            btnClose?.setOnClickListener {
+                dialog.dismiss()
+            }
+
+            dialog.show()
+        }
+
 //        Glide.with(holder.itemView)
 //                .load("${data.gambar}")
 //                .apply(RequestOptions().transform(CenterCrop(), RoundedCorners(20)))
@@ -58,6 +99,8 @@ class RecyclerViewInfoTerkini(
     }
 
     class MyViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        var judul: TextView = itemView.findViewById(R.id.tvInfoTerkini)
         var gambarInfo: ImageView = itemView.findViewById(R.id.ivInfoTerkini)
+        var layoutInfoTerkini: ConstraintLayout = itemView.findViewById(R.id.layoutInfoTerkini)
     }
 }
--- app/src/main/java/com/maluku/sma_rt/view/warga/adapter/RecyclerViewKegiatanWarga.kt
+++ app/src/main/java/com/maluku/sma_rt/view/warga/adapter/RecyclerViewKegiatanWarga.kt
@@ -1,16 +1,23 @@
 package com.maluku.sma_rt.view.warga.adapter
 
 import android.content.ContentValues
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.Window
+import android.widget.ImageButton
 import android.widget.ImageView
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.CenterCrop
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.bumptech.glide.request.RequestOptions
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.FirebaseStorage
 import com.maluku.sma_rt.R
@@ -34,6 +41,7 @@ class RecyclerViewKegiatanWarga(
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         val data = listKegiatan[position]
+        holder.judul.text = data.judul.toString()
 //        Firebase Storage
         val storageRef = FirebaseStorage.getInstance().reference.child("images/${data.gambar}")
         Log.d(ContentValues.TAG,"Adapter get ref image: $storageRef")
@@ -47,6 +55,38 @@ class RecyclerViewKegiatanWarga(
 
         }
 
+        holder.layoutKegiatan.setOnClickListener { view ->
+            val dialog = BottomSheetDialog(view.context, R.style.AppBottomSheetDialogTheme)
+            dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+            dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+            dialog.setContentView(R.layout.bottomsheet_detailinformasi)
+
+            val judul = dialog.findViewById<TextView>(R.id.namainformasi)
+            val gambar = dialog.findViewById<ImageView>(R.id.gambarinformasi)
+            val deskripsi = dialog.findViewById<TextView>(R.id.deskripsiinformasi)
+
+            judul!!.text = data.judul.toString()
+            deskripsi!!.text = data.detail.toString()
+            val storageRef = FirebaseStorage.getInstance().reference.child("images/${data.gambar}")
+            Log.d(ContentValues.TAG,"Adapter get ref image: $storageRef")
+            val localFile = File.createTempFile("tempFile","jpg")
+            storageRef.getFile(localFile).addOnSuccessListener {
+                Glide.with(holder.itemView)
+                    .load(localFile.path)
+                    .apply(RequestOptions().transform(CenterCrop(), RoundedCorners(20)))
+                    .into(gambar!!)
+            }.addOnFailureListener {
+            }
+
+            val btnClose = dialog.findViewById<ImageButton>(R.id.btn_close)
+
+            btnClose?.setOnClickListener {
+                dialog.dismiss()
+            }
+
+            dialog.show()
+        }
+
 //        Glide.with(holder.itemView)
 //            .load("${data.gambar}")
 //            .apply(RequestOptions().transform(CenterCrop(), RoundedCorners(20)))
@@ -58,7 +98,9 @@ class RecyclerViewKegiatanWarga(
     }
 
     class MyViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        var judul: TextView = itemView.findViewById(R.id.tvKegiatanWarga)
         var gambarKegiatan: ImageView = itemView.findViewById(R.id.ivKegiatan)
+        var layoutKegiatan: ConstraintLayout = itemView.findViewById(R.id.layoutKegiatan)
     }
 
 }
