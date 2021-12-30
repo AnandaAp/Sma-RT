5e0806394c04f0b238d2b2a231875e964e17c0fe
abghifareihand <info.abghifareihan@gmail.com>
2021-12-31 01:34:45 +0700

    Update: bottomsheet daftarproduk/terlaris

--- app/src/main/java/com/maluku/sma_rt/view/warga/adapter/RecyclerViewTerlaris.kt
+++ app/src/main/java/com/maluku/sma_rt/view/warga/adapter/RecyclerViewTerlaris.kt
@@ -1,17 +1,23 @@
 package com.maluku.sma_rt.view.warga.adapter
 
 import android.content.ContentValues
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.Window
+import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.CenterCrop
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.bumptech.glide.request.RequestOptions
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.firebase.storage.FirebaseStorage
 import com.maluku.sma_rt.R
 import com.maluku.sma_rt.model.keluarga.GetAllKeluargaItem
@@ -59,7 +65,43 @@ class RecyclerViewTerlaris(
 
         }
 
+        holder.layoutDaftarproduk.setOnClickListener { view ->
+            val dialog = BottomSheetDialog(view.context, R.style.AppBottomSheetDialogTheme)
+            dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+            dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+            dialog.setContentView(R.layout.bottomsheet_detailproduk)
+
+            val nama = dialog.findViewById<TextView>(R.id.namaproduk)
+            val gambar = dialog.findViewById<ImageView>(R.id.gambarproduk)
+            val harga = dialog.findViewById<TextView>(R.id.hargaproduk)
+            val deskripsi = dialog.findViewById<TextView>(R.id.deskripsiproduk)
+
+            nama!!.text = data.nama.toString()
+            harga!!.text = data.harga.toString()
+            deskripsi!!.text = data.detail.toString()
+            val storageRef = FirebaseStorage.getInstance().reference.child("images/${data.gambar}")
+            Log.d(ContentValues.TAG,"Adapter get ref image: $storageRef")
+            val localFile = File.createTempFile("tempFile","jpg")
+            storageRef.getFile(localFile).addOnSuccessListener {
+                Glide.with(holder.itemView)
+                    .load(localFile.path)
+                    .apply(RequestOptions().transform(CenterCrop(), RoundedCorners(20)))
+                    .into(gambar!!)
+            }.addOnFailureListener {
+            }
+
+            val btnClose = dialog.findViewById<ImageButton>(R.id.btn_close)
+
+            btnClose?.setOnClickListener {
+                dialog.dismiss()
+            }
+
+            dialog.show()
+        }
+
     }
+
+
 
     override fun getItemCount(): Int {
         return listProduk.size
@@ -70,6 +112,7 @@ class RecyclerViewTerlaris(
         var hargaBarang: TextView = itemView.findViewById(R.id.tv_hargabarang)
         var namaToko: TextView = itemView.findViewById(R.id.tv_namatoko)
         var gambarBarang: ImageView = itemView.findViewById(R.id.imageBarang)
+        var layoutDaftarproduk: ConstraintLayout = itemView.findViewById(R.id.layoutDaftarproduk)
     }
 
     private fun toRupiah(number: Double): String{
